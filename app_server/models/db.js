b12d03f99e37d98262403017ae07dc0433c3ca1c
Artem <artem.valiev@gmail.com>
2016-06-02 08:17:25 +0000

    ss

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
+
 if (process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGOLAB_URI;
 }
-
 mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
@@ -45,5 +45,5 @@ process.on('SIGTERM', function() {
     });
 });
 
-// BRING IN YOUR SCHEMAS & MODELS
+// BRING IN YOUR SCHEMAS & MODELS!
 require('./locations');
